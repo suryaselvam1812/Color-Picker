53b7361e51896238e2d79a81f39f0d2b3814826d
suryaselvam1812 <suryaselvam1812@gmail.com>
2024-09-23 11:49:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function color_change_1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    let value = document.getElementById("selectedColorHexCode")
+    value.textContent = "#e0e0e0"
+}
+
+function color_change_2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    let value = document.getElementById("selectedColorHexCode")
+    value.textContent = "#6fcf97"
+}
+
+function color_change_3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    let value = document.getElementById("selectedColorHexCode")
+    value.textContent = "#56ccf2"
+}
+
+function color_change_4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    let value = document.getElementById("selectedColorHexCode")
+    value.textContent = "#bb6bd9"
+}
